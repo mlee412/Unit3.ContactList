8578b4ecc3f01559b0c90dec220dfd7bd477a232
Moses Lee <molee@Mosess-MBP.fios-router.home>
2023-12-18 21:07:14 -0500

    finished setting up the table

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import ContactList from './components/ContactList'
+import './App.css'
+
+export default function App() {
+
+  return (
+    <>
+      <ContactList />
+    </>
+  )
+}
+
